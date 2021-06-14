6a0c8a94543e884127c38ea6ac4d5012840668dc
Bas Vreeken <basvreeken@hotmail.com>
2021-06-14 08:37:01 +0200

    Completed priming and staging

--- AirCampusTask/AirCampusTask/Repositories/LearningTaskRepository.cs
+++ AirCampusTask/AirCampusTask/Repositories/LearningTaskRepository.cs
@@ -105,12 +105,340 @@ namespace AirCampusTask.Repositories
                 });
                 ttnTasks.Add(new LearningTask()
                 {
-                    Category = "Installeer storage", Title = "Installeer de firmware-updates opde HP MSA.",
+                    Category = "Installeer storage", Title = "Installeer de firmware-updates op de HP MSA.",
                     Instruction =
                         "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.3.2"
                 });
-                //ttnTasks.Add(new LearningTask() {Category = "", Title = "", Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf"});
-
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Server Priming",
+                    Title = "Algemene informatie.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst; 5.4"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Server Priming",
+                    Title = "Installeer de updates op de HP ProLiant-server (indien benodigd).",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.4.1.1"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Server Priming",
+                    Title = "Test de hardware van de HP ProLiant-server.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.4.1.2"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Server Priming",
+                    Title = "Reset de ROM Based Setup.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.4.1.3"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Server Priming",
+                    Title = "Configureer de ROM Based Setup",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.4.1.4 of 5.4.1.5"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "ESXi-deployment",
+                    Title = "Algemene informatie.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.5"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "ESXi-deployment",
+                    Title = "Creeer een opstartbare USB-stick voor de installatie van VMware ESXi.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.5.1"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "ESXi-deployment",
+                    Title = "Installeer VMware ESXi op de host.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.52"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "USI-deploy",
+                    Title = "Algemene informatie.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf,. Hfdst 5.6"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "USI-deploy",
+                    Title = "Tijdcontrole Red Router - MWS.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.6"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "USI-deploy",
+                    Title = "Voer de uitrol van de USI-server uit.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.6.2"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "USI-deploy",
+                    Title = "Configureer de USI-server",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.6.3"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "VM-builder",
+                    Title = "Algemene informatie.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.7"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "VM-builder",
+                    Title = "Controleer de tijd van de USI-server.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.7"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "VM-builder",
+                    Title = "Start de TITAAN VM-builder op de USI-server met INFRA.XML",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.7.1"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "VM-builder",
+                    Title = "Start de TITANA s=Startup Manager op de USI-server.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.7.2"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "VM-builder",
+                    Title = "Controleer en monitor met de USI Monitor de voortgang.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.7.1"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "VM-builder",
+                    Title = "Installatie gereed? Start de TITAAN Startup Manager om alle VM's te rebooten.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.7.2"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "VM-builder",
+                    Title = "Controleer of de Call Manager een Join Domain heeft gedaan.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.7.2"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Priming & Staging-accounts",
+                    Title = "Algemene informatie.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.8"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Priming & Staging-accounts",
+                    Title = "Enable Priming & staging-account (PS account) per CISNode.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.8.1"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Orchestrator",
+                    Title = "Algemene informatie.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.9"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Orchestrator",
+                    Title = "Importeer de Orchestrator runbooks.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.9.1"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Orchestrator",
+                    Title = "Start de SC Orchestrator -> CIS Admins runbook (alleen op primaire CISNode).",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.9.2"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Post-Configuratie SCCM",
+                    Title = "Algemene informatie.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.10"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Post-Configuratie SCCM",
+                    Title = "Start de Configuration Manager-console.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.10.1"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Post-Configuratie SCCM",
+                    Title = "Configureer de default client settings",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.10.2"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Post-Configuratie SCCM",
+                    Title = "Creeer de applicaties in Configuration Manager.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.10.3"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Client",
+                    Title = "Algemene informatie.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.11 en 5.11.1"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Client",
+                    Title = "Configureer MDT.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.11.2"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Client",
+                    Title = "Voeg het MWS toe aan het domein.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.11.6"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Afronding installatie",
+                    Title = "Algemene informatie.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 6"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Afronding installatie",
+                    Title = "Verwidjer het CIS Plan met de naam INFRA.xml",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 6.1"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Afronding installatie",
+                    Title = "Ontkoppel de USI-cd images",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 6.2"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Afronding installatie",
+                    Title = "Verwijder de USI-cd ISO uit de datastore.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 6.3"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Afronding installatie",
+                    Title = "Maak een back-up van de USI-server.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 6.4.1"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Afronding installatie",
+                    Title = "Verwijder de USI-server.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 6.4.2"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Provisioning",
+                    Title = "Algemene informatie.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 7"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Provisioning",
+                    Title = "Voer de staging van TCTS uit.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 7.1"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Provisioning",
+                    Title = "Voer de Unit provisioning (OC) uit.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 7.2"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Provisioning",
+                    Title = "Voer de Unit provisioning (Eenheden) uit.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 7.2"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Provisioning",
+                    Title = "Verwijder de Merkingen en Rubriceringen Mail CLient.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 7.3"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Provisioning",
+                    Title = "Rond provisioning af.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 7.4"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Provisioning",
+                    Title = "Werk EnPoint protection definition-updates bij.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 7.5"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Provisioning",
+                    Title = "TITAAN Security Updates",
+                    Instruction = "TITAAN-disk:\\\\TTN43\\\\Security Update"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Client Deployment",
+                    Title = "Deploy clients.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.11.3"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Client Deployment",
+                    Title = "Encrypt clients met verwijderbare harddisks(s) met BitLocker (optioneel).",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 5.11.4 en 5.11.5"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Back-Up",
+                    Title = "Algemene informatie.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 8"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Back-Up",
+                    Title = "Indicatoren en aansluitingen NAS.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 8.1"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Back-Up",
+                    Title = "Configureer de back-up storage.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 8.2"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Back-Up",
+                    Title = "Installeer Acronis Backup.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 8.3"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Back-Up",
+                    Title = "Configureer de back-ups.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 8.4"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "Back-Up",
+                    Title = "Configureer de replicatie.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 8.5"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "UPS configureren",
+                    Title = "Algemene informatie.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 9"
+                });
+                ttnTasks.Add(new LearningTask()
+                {
+                    Category = "UPS configureren",
+                    Title = "Configureer een enkelvoudige UPS.",
+                    Instruction = "Lees de stappen in het document TITAAN 4.3 - Platforminstallatie - SIM.pdf, Hfdst 9.1"
+                });
                 await _connection.InsertAllAsync(ttnTasks);
             }
             
